e35aab531052dee908b75ee68cce5ede3b007180
Jack Marchant <jack@expert360.com>
2018-04-23 21:38:13 +1000

    package info

--- mix.exs
+++ mix.exs
@@ -6,10 +6,16 @@ defmodule ElixirCms.MixProject do
       app: :elixir_cms,
       version: "0.1.0",
       elixir: "~> 1.6",
+      source_url: "https://github.com/jackmarchant/elixir_cms",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -23,6 +29,29 @@ defmodule ElixirCms.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Jack Marchant"
+      ],
+      licenses: [
+        "MIT"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/jackmarchant/elixir_cms",
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: "https://github.com/jackmarchant/elixir_cms",
+      source_ref: "v#{@version}"
     ]
   end
 end
