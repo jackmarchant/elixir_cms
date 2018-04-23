6c4fd5e9abafb6663720cefb3cfbb4a3e7f65614
Jack Marchant <jack@expert360.com>
2018-04-23 21:40:45 +1000

    additions

--- mix.exs
+++ mix.exs
@@ -1,17 +1,27 @@
 defmodule ElixirCms.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :elixir_cms,
       version: "0.1.0",
       elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
       source_url: "https://github.com/jackmarchant/elixir_cms",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package(),
+      docs: docs(),
     ]
+  end
+
+  defp description do
+    """
+    Elixir CMS - a developer friendly content management system.
+    """
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -27,8 +37,7 @@ defmodule ElixirCms.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
