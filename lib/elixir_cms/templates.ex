f95701b250d6eab5308bce38441b015ee554d61f
Jack Marchant <jack@expert360.com>
2018-04-23 22:53:24 +1000

    fix

--- lib/elixir_cms/templates.ex
+++ lib/elixir_cms/templates.ex
@@ -1,6 +1,6 @@
 defmodule ElixirCMS.Templates do
   @moduledoc """
-  Templates to serve up content in the Admin
+  Generate templates and provide them with helper functions
   """
 
   require EEx
@@ -12,5 +12,9 @@ defmodule ElixirCMS.Templates do
 
   for {fn_name, file_name} <- @templates do
     EEx.function_from_file :def, fn_name, Path.expand("./templates/#{file_name}.html.eex", __DIR__), [:assigns]
+  end
+
+  def path(conn, path) do
+    Path.join(conn.assigns[:namespace], path)
   end
 end
